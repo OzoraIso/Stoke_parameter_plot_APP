edef02dd0952fe0779a5f52f15c0c75a71b4b0f3
OzoraIso <ootyanfmarinos04@gmail.com>
2022-08-29 00:19:04 +0900

    poincare_noclass.py

---
+++ poincare_noclass.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import mysql.connector
+import csv # comma separated valules
+
+
+
+root = Tk()
+root.title("Stokes Paramter plotter")
+root.geometry("400x650")
+
+
+
+root.mainloop()
